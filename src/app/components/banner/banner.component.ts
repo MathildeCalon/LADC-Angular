1c4548b85cb06925e325ecd6a358fcb42e0795fc
Mathilde Calon <mathilde.calon@gmail.com>
2024-10-03 10:27:56 +0200

    banner OK

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -5,8 +5,42 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.css'
+  styleUrl: './banner.component.css',
 })
 export class BannerComponent {
+  banner: string[] = [
+    'bandeau_slogan.png',
+    'bandeau_bartop.png'
+  ];
 
+  currentImage: number = 0;
+  interval: any;
+
+  ngOnInit():void {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy():void {
+   if(this.interval)
+    clearInterval(this.interval);
+  }
+
+  startAutoSlide():void {
+    this.interval = setInterval(() => {
+      this.nextImage();
+    }, 5000);
+  }
+
+  stopAutoSlide():void {
+    if(this.interval)
+      clearInterval(this.interval);
+  }
+
+  nextImage():void {
+    this.currentImage = (this.currentImage + 1) % this.banner.length;
+  }
+
+  prevImage():void {
+    this.currentImage = (this.currentImage - 1 + this.banner.length) % this.banner.length;
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { NavComponent } from '../../components/nav/nav.component';
 import { ArticleComponent } from '../../components/article/article.component';
 import { Article } from '../../utils/types';
 import { ArticlesService } from '../../services/articles.service';
+import { BannerComponent } from "../../components/banner/banner.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, ArticleComponent],
+  imports: [NavComponent, ArticleComponent, BannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
